f21f757ee6fcc07909079ec1b40da2146aa36bd5
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-03-16 11:09:51 -0400

    commit tree ba5c582cef661aa78080596b925a4b9ee27841c5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    def initialize word
+        @word = word
+    end
+    def match (array)
+        chars = @word.chars.sort
+        array.filter{|str| str.chars.sort == chars}
+    end
+end
+
